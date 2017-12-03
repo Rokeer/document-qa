c79cdbad713a3bfe14b9c0fa8331f832a415d1f9
Rokeer <me@colinzhang.com>
2017-12-03 15:20:01 -0500

    fix cont

--- docqa/trainer.py
+++ docqa/trainer.py
@@ -247,7 +247,7 @@ def continue_training(
     """ Train an already existing model, or start for scatch """
     if not exists(out.dir) or os.listdir(out.dir) == 0:
         print("Files do not exist, start from scratch")
-        start_training(data, model, train_params, evaluators, out, notes, dry_run)
+        start_training(data, model, train_params, evaluators, out, notes)
     else:
         print("Files already exist, loading most recent model")
         resume_training_with(data, out, train_params, evaluators, notes, dry_run)
